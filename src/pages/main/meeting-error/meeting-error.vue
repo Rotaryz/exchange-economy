d2ec384f87c5dea526ac73548020906155fba74a
wengtianliang <wengtianliang@jkweixin.com>
2019-09-30 11:30:00 +0800

    添加会议异常页面

--- src/pages/main/meeting-error/meeting-error.vue
+++ src/pages/main/meeting-error/meeting-error.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="meeting-error">
     <navigation-bar :showArrow="false" title="5G新营销"></navigation-bar>
+    <img src="./pic-zanwu@2x.png" alt="" class="meeting-img">
+    <p class="meeting-text">会议排期中，敬请期待</p>
+    <div class="btn" @click="_refresh">返回首页</div>
   </div>
 </template>
 
@@ -8,8 +11,10 @@
   // import * as Helpers from './modules/helpers'
   // import API from '@api'
   import NavigationBar from '@components/navigation-bar/navigation-bar'
+  import app from '@src/app.json'
 
   const PAGE_NAME = 'MEETING_ERROR'
+  const TAB_BAR = app.tabBar.list
 
   export default {
     name: PAGE_NAME,
@@ -24,7 +29,9 @@
       // ...Helpers.computed,
     },
     methods: {
-      // ...Helpers.methods,
+      _refresh() {
+        wx.switchTab({ url: '/' + TAB_BAR[0].pagePath })
+      }
     }
   }
 </script>
@@ -34,4 +41,25 @@
 
   .meeting-error
     width: 100%
+  .meeting-img
+    width: 109px
+    height: @width
+    display: block
+    margin: 100px auto 12px
+  .meeting-text
+    font-family: $font-family-regular
+    color: #888
+    font-size: $font-size-14
+    text-align: center
+    margin-bottom: 30px
+  .btn
+    border-radius: 20.5px
+    width: 140px
+    height: 41px
+    line-height: 41px
+    text-align: center
+    color: $color-white
+    background: $color-main
+    font-size: $font-size-14
+    margin: 23.5px auto
 </style>
